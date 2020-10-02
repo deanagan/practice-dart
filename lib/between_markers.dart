5cea41a9665a1a5fbeda102ee3aca18ebe0b8d54
Dean <agandfr@gmail.com>
2020-10-02 18:00:19 +1000

    Update between_markers.dart

--- lib/between_markers.dart
+++ lib/between_markers.dart
@@ -5,7 +5,7 @@ class BetweenMarkers {
   BetweenMarkers(this.left_marker, this.right_marker);
 
   String GetValue(String input) {
-    //return line.slice(line.indexOf(left) + 1, line.indexOf(right))
+    
     return input.substring(
         input.indexOf(this.left_marker) + 1, input.indexOf(right_marker));
   }
