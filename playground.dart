c9aad0367ec93f757ab9a1da16da83ced7b7b2fe
Dean Agan <agandfr@gmail.com>
2020-10-03 15:47:53 +1000

    remove unused file

--- playground.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  var input = "[hello]";
-  var left_marker = "[";
-  var right_marker = "]";
-
-  print(input.substring(
-      input.indexOf(left_marker) + 1, input.indexOf(right_marker)));
-}
