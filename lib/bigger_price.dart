8c3acac93f9730a1101d5be9dcd0102d76db149f
Dean Agan <agandfr@gmail.com>
2020-10-06 18:06:00 +1100

    Adding bare implementation for price analyst

---
+++ lib/bigger_price.dart
@@ -0,0 +1,11 @@
+class Stock {
+  String name;
+  int price;
+  Stock(this.name, this.price);
+}
+
+class PriceAnalyst {
+  List<Stock> biggerPrice(int limit, List<Stock> stocks) {
+    return new List<Stock>();
+  }
+}
