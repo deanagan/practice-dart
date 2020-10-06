1e16cfb5a07abb7e8f317347fa20c6c665f06e33
Dean Agan <agandfr@gmail.com>
2020-10-07 09:49:48 +1100

    Implement solution for bigger_price

--- lib/bigger_price.dart
+++ lib/bigger_price.dart
@@ -1,11 +1,52 @@
+library bigger_price;
+
+import 'package:quiver/core.dart';
+
 class Stock {
   String name;
   int price;
   Stock(this.name, this.price);
+
+  @override
+  String toString() {
+    return 'Stock: $name Price \$$price';
+  }
+
+  @override
+  bool operator ==(other) =>
+      this.name == other.name && this.price == other.price;
+
+  @override
+  int get hashCode => hash2(name.hashCode, price.hashCode);
 }
 
 class PriceAnalyst {
   List<Stock> biggerPrice(int limit, List<Stock> stocks) {
-    return new List<Stock>();
+    stocks.sort((s1, s2) => s2.price.compareTo(s1.price));
+    return stocks.take(limit).toList();
   }
 }
+
+// main() {
+//   var sut = new PriceAnalyst();
+//   Function deepEq = const DeepCollectionEquality().equals;
+//   // Acts
+//   final result = sut.biggerPrice(2, [
+//     new Stock("bread", 100),
+//     new Stock("wine", 138),
+//     new Stock("meat", 15),
+//     new Stock("water", 1)
+//   ]);
+//   // Asserts
+//   var match = deepEq(result, [
+//     new Stock("wine", 138),
+//     new Stock("bread", 100),
+//   ]);
+//   print(match);
+//   print(result);
+//   print(IterableEquality().equals([
+//     new Stock("wine", 138),
+//     new Stock("bread", 100),
+//   ], result));
+
+// }
