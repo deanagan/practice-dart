65be62dbc752ca9a8eb010a8f1b91d328609cbf0
Dean Agan <agandfr@gmail.com>
2020-10-16 20:53:15 +1100

    Add solution to fix

--- lib/three_words.dart
+++ lib/three_words.dart
@@ -1,7 +1,19 @@
 library three_words;
 
+extension NumberParsing on String {
+  bool isAlpha() {
+    String source = this.trim();
+    return int.tryParse(source) == null;
+  }
+}
+
 class WordAnalyzer {
   bool has3ConsecutiveWords(String text) {
-    return true;
+    var words = text.split(' ');
+    var len = words.fold(
+        0,
+        (total, currentWord) =>
+            total + ((currentWord.isAlpha() || total >= 3) ? 1 : -total));
+    return len >= 3;
   }
 }
