b797a0a1e314166d010fac466f561eacfd0b2e9e
Dean Agan <agandfr@gmail.com>
2020-10-16 20:17:42 +1100

    Add 3 words test

---
+++ test/three_words_test.dart
@@ -0,0 +1,33 @@
+import "package:test/test.dart";
+import "package:practicedart/three_words.dart";
+
+class TestParameter {
+  String text;
+  bool isThreeOrMore;
+
+  TestParameter(this.text, this.isThreeOrMore);
+
+  @override
+  String toString() {
+    return """The text: $text ${isThreeOrMore ? 'has' : 'has no'} 3 consecutive non digit words.""";
+  }
+}
+
+main() {
+  group("Three words test", () {
+    List<TestParameter> testParameters = [
+      new TestParameter("Hello World hello", true),
+    ];
+
+    testParameters.forEach((tp) {
+      test("Expect ${tp}", () {
+        // Arrange
+        var sut = new WordAnalyzer();
+        // Acts
+        final result = sut.has3ConsecutiveWords(tp.text);
+        // Asserts
+        expect(result, tp.isThreeOrMore);
+      });
+    });
+  });
+}
