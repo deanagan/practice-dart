6401f622c98eb37be181e3aba0bf9b82f137cd97
Dean Agan <agandfr@gmail.com>
2020-10-17 20:20:58 +1100

    Add test with extra spaces for reverse

--- test/backward_string_by_word_test.dart
+++ test/backward_string_by_word_test.dart
@@ -7,6 +7,7 @@ void main() {
       '': '',
       'world': 'dlrow',
       'hello world': 'olleh dlrow',
+      'hello   world': 'olleh   dlrow',
     };
 
     testParametersToExpectedResult.forEach((input, expected) {
