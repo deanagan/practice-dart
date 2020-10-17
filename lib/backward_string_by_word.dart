0a1d69b0ff2a661a21de3a15a1c9cc616a37411c
Dean Agan <agandfr@gmail.com>
2020-10-17 20:20:25 +1100

    soln to pass multiple word reversal

--- lib/backward_string_by_word.dart
+++ lib/backward_string_by_word.dart
@@ -2,6 +2,6 @@ library backward_string_by_word;
 
 class BackwardStringByWord {
   String ReverseByWord(String input) {
-    return input.split('').reversed.join();
+    return input.split(' ').map((e) => e.split('').reversed.join('')).join(' ');
   }
 }
