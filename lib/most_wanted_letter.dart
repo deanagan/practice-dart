013a6265d393b34cc02c14de65fbc334d210ead2
Dean Agan <agandfr@gmail.com>
2020-10-24 20:40:53 +1100

    Add solution

--- lib/most_wanted_letter.dart
+++ lib/most_wanted_letter.dart
@@ -1,7 +1,24 @@
 library most_wanted_letter;
 
+extension StringCounter on List<String> {
+  int countOccurenceInsensitive(String s) {
+    return this.where((ch) => ch == s).length;
+  }
+}
+
 class MostWantedLetter {
   String getMostWanted(String subject) {
-    return "";
+    final splitSubject = subject.toLowerCase().split('');
+    final counter = new Map.fromIterable(splitSubject,
+        key: (item) => item,
+        value: (item) => splitSubject.countOccurenceInsensitive(item));
+    var mwl = splitSubject.first;
+    counter.forEach((k, v) {
+      if (counter[mwl] <= v) {
+        mwl = k;
+      }
+    });
+
+    return mwl;
   }
 }
