d23b9980b10e9f6035b00abd936990d89fb58375
Dean Agan <agandfr@gmail.com>
2020-11-05 11:37:41 +1100

    Implement test

---
+++ test/number_pairs_test.dart
@@ -0,0 +1,36 @@
+import "package:test/test.dart";
+import "package:practicedart/number_pairs.dart";
+
+class TestParameter {
+  List<int> numbers;
+  int denominator;
+  int expected;
+
+  TestParameter(this.numbers, this.denominator, this.expected);
+
+  @override
+  String toString() {
+    return """
+    total pairs for: $numbers with denominator $denominator to be '$expected'
+    """;
+  }
+}
+
+main() {
+  group("Number pairs test: ", () {
+    List<TestParameter> testParameters = [
+      new TestParameter([5, 25, 45, 15], 30, 2)
+    ];
+
+    testParameters.forEach((tp) {
+      test("Expect ${tp}", () {
+        // Arrange
+        var sut = new NumberPairs(tp.numbers);
+        // Acts
+        final result = sut.getTotalNumberPairs(tp.denominator);
+        // Asserts
+        expect(result, tp.expected);
+      });
+    });
+  });
+}
