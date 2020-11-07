f299297ee7e5276b31e4d6d0593b0a4b3a701cfa
Dean Agan <agandfr@gmail.com>
2020-11-07 20:29:38 +1100

    Pass the test.

--- lib/split_string.dart
+++ lib/split_string.dart
@@ -1,11 +1,21 @@
 library split_string;
 
+extension StringFiller on String {
+  String getFirstTwoOrFill() {
+    return this.length >= 2 ? this.substring(0, 2) : this + '_';
+  }
+}
+
 class StringSplitter {
   int splitBy;
 
   StringSplitter({this.splitBy = 2});
 
   List<String> split(String input) {
-    return ['ab', 'cd'];
+    var result = input.isEmpty ? [] : [input.getFirstTwoOrFill()];
+    if (input.length > 2) {
+      result.addAll(split(input.substring(2)));
+    }
+    return result;
   }
 }
