e8f14b2a50e6849027fd64203b82a664871bfb76
Dean Agan <agandfr@gmail.com>
2020-11-07 19:45:00 +1100

    Add test

---
+++ test/split_string_test.dart
@@ -0,0 +1,35 @@
+import "package:test/test.dart";
+import "package:practicedart/split_string.dart";
+
+class TestParameter {
+  String input;
+  List<String> expected;
+
+  TestParameter(this.input, this.expected);
+
+  @override
+  String toString() {
+    return """
+        The input: $input split by 2 results in $expected
+        """;
+  }
+}
+
+void main() {
+  group("Split string test: ", () {
+    List<TestParameter> testParameters = [
+      new TestParameter('abcd', ['ab', 'cd']),
+    ];
+
+    testParameters.forEach((tp) {
+      test("Expect ${tp}", () {
+        // Arrange
+        var sut = new StringSplitter();
+        // Acts
+        final result = sut.split(tp.input);
+        // Asserts
+        expect(result, tp.expected);
+      });
+    });
+  });
+}
