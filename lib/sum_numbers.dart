998337fdcbbd3c8c7df5b6d301f02537058afec1
Dean Agan <agandfr@gmail.com>
2020-11-08 17:21:52 +1100

    Refactor solution

--- lib/sum_numbers.dart
+++ lib/sum_numbers.dart
@@ -2,8 +2,13 @@ library sum_numbers;
 
 class SumNumbers {
   int total(String text) {
-    final exp = new RegExp(r"(^| )(\d)+($| )");
-    return exp.allMatches(text).fold(0,
-        (total, value) => int.parse(value.group(0), onError: (s) => 0) + total);
+    // Keeping solution below as reference for using regular expression
+    // final exp = new RegExp(r"(^| )(\d)+($| )");
+    // return exp.allMatches(text).fold(0,
+    //     (total, value) => int.parse(value.group(0), onError: (s) => 0) + total);
+
+    return text
+        .split(' ')
+        .fold(0, (total, val) => int.parse(val, onError: (s) => 0) + total);
   }
 }
