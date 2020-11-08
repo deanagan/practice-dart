97824a9a3ce0689e8a0bf6ee9c3d4ab0dbbca75e
Dean Agan <agandfr@gmail.com>
2020-11-08 17:08:06 +1100

    Add solution to pass

--- lib/sum_numbers.dart
+++ lib/sum_numbers.dart
@@ -1,5 +1,9 @@
+library sum_numbers;
+
 class SumNumbers {
   int total(String text) {
-    return 0;
+    final exp = new RegExp(r" (\d)+($| )");
+    return exp.allMatches(text).fold(0,
+        (total, value) => int.parse(value.group(0), onError: (s) => 0) + total);
   }
 }
