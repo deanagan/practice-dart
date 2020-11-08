b649ca224d27f120dc0b22f5a9db9a0f4e6bf804
Dean Agan <agandfr@gmail.com>
2020-11-08 17:12:06 +1100

    Change regexp to pass new test case.

--- lib/sum_numbers.dart
+++ lib/sum_numbers.dart
@@ -2,7 +2,7 @@ library sum_numbers;
 
 class SumNumbers {
   int total(String text) {
-    final exp = new RegExp(r" (\d)+($| )");
+    final exp = new RegExp(r"(^| )(\d)+($| )");
     return exp.allMatches(text).fold(0,
         (total, value) => int.parse(value.group(0), onError: (s) => 0) + total);
   }
