a9c848377e2dbebcaa65bd578e7eb575805f1127
Dean Agan <agandfr@gmail.com>
2020-11-17 19:37:05 +1100

    Updated tests to avoid dynamic[] to int[] issues

--- test/reverse_ascending_test.dart
+++ test/reverse_ascending_test.dart
@@ -1,27 +1,41 @@
 import "package:test/test.dart";
 import "../lib/sorter.dart";
 
+class TestParameter {
+  List<int> input;
+  List<int> expected;
+
+  TestParameter(this.input, this.expected);
+
+  @override
+  String toString() {
+    return """
+        The input: $input will return $expected
+        """;
+  }
+}
+
 void main() {
   group("Reverse ascending sort test:", () {
-    var testParametersToExpectedResult = {
-      [1, 2, 3, 4, 5]: [5, 4, 3, 2, 1],
-      [5, 7, 10, 4, 2, 7, 8, 1, 3]: [10, 7, 5, 4, 8, 7, 2, 3, 1],
-      [5, 4, 3, 2, 1]: [5, 4, 3, 2, 1],
-      []: [],
-      [1]: [1],
-      [1, 1]: [1, 1],
-      [1, 1, 2]: [1, 2, 1],
-      [1, 2, 2, 3]: [2, 1, 3, 2],
+    var testParameters = {
+      TestParameter([1, 2, 3, 4, 5], [5, 4, 3, 2, 1]),
+      TestParameter([5, 7, 10, 4, 2, 7, 8, 1, 3], [10, 7, 5, 4, 8, 7, 2, 3, 1]),
+      TestParameter([5, 4, 3, 2, 1], [5, 4, 3, 2, 1]),
+      TestParameter([], []),
+      TestParameter([1], [1]),
+      TestParameter([1, 1], [1, 1]),
+      TestParameter([1, 1, 2], [1, 2, 1]),
+      TestParameter([1, 2, 2, 3], [2, 1, 3, 2]),
     };
 
-    testParametersToExpectedResult.forEach((input, expected) {
-      test("$input should return $expected", () {
+    testParameters.forEach((tp) {
+      test("Expect $tp", () {
         // Arrange
         var sut = new Sorter();
         // Acts
-        final result = sut.sortReverseAscending(input);
+        final result = sut.sortReverseAscending(tp.input);
         // Asserts
-        expect(result, expected);
+        expect(result, tp.expected);
       });
     });
   });
