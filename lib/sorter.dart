cbd6d2c6b234c923b9447b0b17f89f041dea268f
Dean Agan <agandfr@gmail.com>
2020-11-17 19:37:16 +1100

    Add solution

---
+++ lib/sorter.dart
@@ -0,0 +1,32 @@
+library sorting_utility;
+
+import 'package:stack/stack.dart';
+
+extension StackPopper on Stack<int> {
+  Iterable<int> popAll() sync* {
+    while (this.isNotEmpty) {
+      yield this.pop();
+    }
+  }
+}
+
+class Sorter {
+  List<int> sortReverseAscending(List<int> numbers) {
+    var stack = Stack<int>();
+    var result = List<int>();
+
+    if (numbers.isEmpty) {
+      return result;
+    }
+
+    numbers.forEach((element) {
+      if (stack.isNotEmpty && stack.top() >= element) {
+        result.addAll(stack.popAll());
+      }
+      stack.push(element);
+    });
+
+    result.addAll(stack.popAll());
+    return result;
+  }
+}
