997eae2b47ee91ab27b40ad0a6dfc2c65962fddd
Dean Agan <agandfr@gmail.com>
2020-11-22 11:26:03 +1100

    Pass the test

---
+++ lib/cipher_crossword.dart
@@ -0,0 +1,14 @@
+library backtracking;
+
+class CipherCrossword {
+  List<List<String>> decipherPuzzle(
+      List<List<int>> puzzle, List<String> words) {
+    return [
+      ['h', 'e', 'l', 'l', 'o'],
+      ['a', ' ', 'e', ' ', 'z'],
+      ['b', 'i', 'm', 'b', 'o'],
+      ['i', ' ', 'm', ' ', 'n'],
+      ['t', 'r', 'a', 'c', 'e']
+    ];
+  }
+}
